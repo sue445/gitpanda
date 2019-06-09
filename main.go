a01c7273358d3ee7ff85e6708355098310baef76
sue445 <sue445@sue445.net>
2019-06-09 13:20:12 +0900

    Don't include lambda packages except linux amd64

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"bytes"
-	"context"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 	"flag"
 	"fmt"
 	"log"
@@ -29,33 +26,6 @@ func init() {
 	flag.BoolVar(&isPrintVersion, "version", false, "Whether showing version")
 
 	flag.Parse()
-}
-
-func main() {
-	if isPrintVersion {
-		printVersion()
-		return
-	}
-
-	checkEnv("GITLAB_API_ENDPOINT")
-	checkEnv("GITLAB_BASE_URL")
-	checkEnv("GITLAB_PRIVATE_TOKEN")
-	checkEnv("SLACK_OAUTH_ACCESS_TOKEN")
-
-	if os.Getenv("LAMBDA_TASK_ROOT") != "" && os.Getenv("LAMBDA_RUNTIME_DIR") != "" {
-		// for AWS Lambda
-		lambda.Start(lambdaHandler)
-		return
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000"
-	}
-
-	fmt.Printf("gitpanda started: port=%s\n", port)
-	http.HandleFunc("/", normalHandler)
-	http.ListenAndServe(":"+port, nil)
 }
 
 func checkEnv(name string) {
@@ -117,34 +87,4 @@ func normalHandler(w http.ResponseWriter, r *http.Request) {
 
 		w.Write([]byte(response))
 	}
-}
-
-func lambdaHandler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	body := strings.TrimSpace(request.Body)
-
-	s := NewSlackWebhook(
-		os.Getenv("SLACK_OAUTH_ACCESS_TOKEN"),
-		&GitLabURLParserParams{
-			APIEndpoint:  os.Getenv("GITLAB_API_ENDPOINT"),
-			BaseURL:      os.Getenv("GITLAB_BASE_URL"),
-			PrivateToken: os.Getenv("GITLAB_PRIVATE_TOKEN"),
-		},
-	)
-	response, err := s.Request(
-		body,
-		false,
-	)
-
-	if err != nil {
-		log.Printf("[ERROR] body=%s, response=%s, error=%v", body, response, err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body: response,
-		}, err
-	}
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusOK,
-		Body: response,
-	}, nil
 }
