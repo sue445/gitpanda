8b4621ec6634fe5dd827f6d7a4f53ba2a4778fd6
sue445 <sue445@sue445.net>
2019-06-30 14:21:28 +0900

    Support group URL

--- gitlab/user_or_group_fetcher.go
+++ gitlab/user_or_group_fetcher.go
@@ -3,6 +3,7 @@ package gitlab
 import (
 	"fmt"
 	"github.com/xanzy/go-gitlab"
+	"net/http"
 	"regexp"
 	"strings"
 	"time"
@@ -20,9 +21,33 @@ func (f *userOrGroupFetcher) fetchPath(path string, client *gitlab.Client, isDeb
 		return nil, nil
 	}
 
-	username := matched[1]
+	name := matched[1]
+	userPage, err := f.fetchUserPath(name, client, isDebugLogging)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if userPage != nil {
+		return userPage, nil
+	}
+
+	groupPage, err := f.fetchGroupPath(name, client, isDebugLogging)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if groupPage != nil {
+		return groupPage, nil
+	}
+
+	return nil, fmt.Errorf("%s is not found", name)
+}
+
+func (f *userOrGroupFetcher) fetchUserPath(name string, client *gitlab.Client, isDebugLogging bool) (*Page, error) {
 	start := time.Now()
-	users, _, err := client.Users.ListUsers(&gitlab.ListUsersOptions{Username: &username})
+	users, _, err := client.Users.ListUsers(&gitlab.ListUsersOptions{Username: &name})
 
 	if err != nil {
 		return nil, err
@@ -30,11 +55,11 @@ func (f *userOrGroupFetcher) fetchPath(path string, client *gitlab.Client, isDeb
 
 	if isDebugLogging {
 		duration := time.Now().Sub(start)
-		fmt.Printf("[DEBUG] userOrGroupFetcher (%s): users=%+v\n", duration, users)
+		fmt.Printf("[DEBUG] fetchUserPath (%s): users=%+v\n", duration, users)
 	}
 
 	if len(users) < 1 {
-		return nil, fmt.Errorf("NotFound user: %s", username)
+		return nil, nil
 	}
 
 	user := users[0]
@@ -49,6 +74,37 @@ func (f *userOrGroupFetcher) fetchPath(path string, client *gitlab.Client, isDeb
 		FooterTitle:            fmt.Sprintf("@%s", user.Username),
 		FooterURL:              fmt.Sprintf("%s/%s", f.baseURL, user.Username),
 		FooterTime:             user.CreatedAt,
+	}
+
+	return page, nil
+}
+
+func (f *userOrGroupFetcher) fetchGroupPath(name string, client *gitlab.Client, isDebugLogging bool) (*Page, error) {
+	start := time.Now()
+	group, _, err := client.Groups.GetGroup(name, func(req *http.Request) error {
+		req.URL.RawQuery = "with_projects=false"
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if isDebugLogging {
+		duration := time.Now().Sub(start)
+		fmt.Printf("[DEBUG] fetchGroupPath (%s): group=%+v\n", duration, group)
+	}
+
+	page := &Page{
+		Title:                  strings.Join([]string{group.Name, "GitLab"}, titleSeparator),
+		Description:            group.Description,
+		AuthorName:             "",
+		AuthorAvatarURL:        "",
+		AvatarURL:              group.AvatarURL,
+		CanTruncateDescription: true,
+		FooterTitle:            fmt.Sprintf("@%s", group.Path),
+		FooterURL:              group.WebURL,
+		FooterTime:             nil,
 	}
 
 	return page, nil
