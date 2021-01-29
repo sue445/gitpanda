296ed82ae199ec18d5c8169717bef3a25911b50d
sue445 <sue445@sue445.net>
2021-01-29 22:41:36 +0900

    Refactor. Use User.WebURL

--- gitlab/user_or_group_fetcher.go
+++ gitlab/user_or_group_fetcher.go
@@ -73,7 +73,7 @@ func (f *userOrGroupFetcher) fetchUserPath(name string, client *gitlab.Client, i
 		AvatarURL:              user.AvatarURL,
 		CanTruncateDescription: true,
 		FooterTitle:            fmt.Sprintf("@%s", user.Username),
-		FooterURL:              fmt.Sprintf("%s/%s", f.baseURL, user.Username),
+		FooterURL:              user.WebURL,
 		FooterTime:             user.CreatedAt,
 	}
 
